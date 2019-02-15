0cd1e4b863ed3ed556a0a9468b307a5945c491b5
Josh Hall <joshhall90@yahoo.com>
2019-02-15 14:52:52 -0500

    heroku deployment ready

---
+++ config.py
@@ -0,0 +1,19 @@
+#  holds all configuration variables for this application here
+#  should not be uploaded to github with sensetive information
+# .gitignore this file
+#  when uploading to heroku, use import as .get() method
+import os
+
+# define the root/base of this project folder
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    # secret key is necessary for forms in flask, it is a security measure to protect against attacks like CSRF
+    # it should never be given out, and should be something that is difficult to break
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # setup database URI, which is the location of our database file/server
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
